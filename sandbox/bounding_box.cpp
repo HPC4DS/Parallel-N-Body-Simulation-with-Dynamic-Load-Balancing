42176bf9090b79dddf7bb0c924c1b114b1636100
Matteo Ranzi <matteoranzi2000@gmail.com>
2025-11-19 18:42:20 +0100

    Update project environment template

--- sandbox/bounding_box.cpp
+++ sandbox/bounding_box.cpp
@@ -2,9 +2,12 @@
 // Created by Matteo Ranzi on 19/11/25.
 //
 
+// #include <mpi.h>
 #include "debug/print_debug.h"
 
 int main(int argc, char *argv[]) {
-    PRINT_DEBUG_INFO("Bounding Box Sandbox\n");
+
+
+
     return 0;
 }
