7ba86aef475038e64eaf7ce297b49a8cc2ce1679
Matteo Ranzi <matteoranzi2000@gmail.com>
2025-11-19 18:08:13 +0100

    Add benchmark tests and integrate GoogleTest framework

---
+++ docs/compilation/How to Compile.md
@@ -0,0 +1,12 @@
+# CMake Options
+// TODO explain the purpose of each option
+- -DREMOTE_RUN
+- -DGLOBAL_PRINT_DEBUG_LEVEL
+- -DBUILD_TESTING
+
+# Environment Variables
+- SANDBOX
+- MPI_RUN
+- WAIT_FOR_DEBUGGER
+
+-> All variables in configs/project.env
---
+++ tests/benchmark_tests.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Matteo Ranzi on 19/11/25.
+//
+
+#include <gtest/gtest.h>
+
+// Demonstrate some basic assertions.
+TEST(BenchmarkTest, todo) {
+    // Expect two strings not to be equal.
+    EXPECT_STREQ("Implement the benchmark test", "here");
+}
